9a50dd9b774b3b0543df2230fdcfdbb31c345da4
SachithaJungkook <727722euit155@skcet.ac.in>
2023-11-07 15:58:08 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import './App.css';
+import Navbar from "./compogitgnent/Navbar";
+import Main from "./component/Main";
+import Footer from "./component/Footer";
+function App() {
+    return (
+      <div>
+      <Navbar/>
+      <Main/>
+      <Footer/>
+      </div>
+    
+    );
+}
+export default App;//Default exports
