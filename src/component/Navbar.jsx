1a65fe3a993599bff14dd54c4df7773c18fcebaf
SachithaJungkook <727722euit155@skcet.ac.in>
2023-11-17 10:23:43 +0530

    form components

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar= () =>{
     return(
         <div>
            <hr></hr>
-           <h1 style={{backgroundColor:"lavender", fontSize:"30px" }}>Your Name</h1>
+           <h1 style={{backgroundColor:"lavender", fontSize:"30px" }}>Sachitha</h1>
            <hr></hr>
         </div>
     );
 };
 
-export default NavBar;
+export default Navbar;
---
+++ src/day3/Submit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Submit = () => {
+    const[name,setName]=useState("");
+
+  return (
+    <from onSubmit=
+    );
+};
+
+export default Submit
